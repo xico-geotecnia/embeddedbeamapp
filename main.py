ab1ad6953c736b94d39cb6654a0d76d7c724c995
marquez85 <fpvsm85@gmail.com>
2022-08-27 03:01:08 +0100

    deploy

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from streamlit_tags import st_tags
+from streamlit_tags import st_tags_sidebar
+from References.pages import main_page,page2
+
+st.set_page_config(layout="wide")
+    
+page_names_to_funcs = {
+"Instructions": main_page,
+"App": page2,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
---
+++ streamlit_app.bat
@@ -0,0 +1,3 @@
+CALL conda.bat activate PLAXIS_V20.4_python37
+streamlit run main.py --server.port 6006
+pause
